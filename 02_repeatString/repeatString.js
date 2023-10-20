355166ef01b0323fb7856c6a9c639405bf8ef4b5
Lukas Broskovic <lukas.broskovic@gmail.com>
2023-10-20 14:45:54 +0200

    Done with first exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+const repeatString = function(string, repeatString) {
+    let i = 0;
+    let finalString = "";
 
+    if (Math.sign(repeatString) === -1) {
+        return "ERROR";
+    } else {
+    
+
+    for (i; i < repeatString; i++) {
+        finalString += string;
+        
+    }
+    return finalString;
+}
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+//define values in fucntion
+// take count in the function and repeat string as many times
+// take the output and join in a single string
